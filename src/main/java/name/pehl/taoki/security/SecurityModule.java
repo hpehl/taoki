d76a7363b7133d6fdbfa5873dedfe9105644e8dc
Harald Pehl <harald.pehl@gmail.com>
2010-01-20 20:45:28 +0000

    Format

--- src/main/java/name/pehl/taoki/security/SecurityModule.java
+++ src/main/java/name/pehl/taoki/security/SecurityModule.java
@@ -1,14 +1,13 @@
 package name.pehl.taoki.security;
 
-
 import org.restlet.resource.ServerResource;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
 
 /**
- * @author $Author:$
- * @version $Revision:$
+ * @author $Author$
+ * @version $Revision$
  */
 public class SecurityModule extends AbstractModule
 {
@@ -17,13 +16,5 @@ public class SecurityModule extends AbstractModule
     {
         SecurityInterceptor interceptor = new SecurityInterceptor();
         bindInterceptor(Matchers.subclassesOf(ServerResource.class), Matchers.annotatedWith(Secured.class), interceptor);
-        // bindInterceptor(Matchers.subclassesOf(ServerResource.class),
-        // Matchers.annotatedWith(Get.class), interceptor);
-        // bindInterceptor(Matchers.subclassesOf(ServerResource.class),
-        // Matchers.annotatedWith(Post.class), interceptor);
-        // bindInterceptor(Matchers.subclassesOf(ServerResource.class),
-        // Matchers.annotatedWith(Put.class), interceptor);
-        // bindInterceptor(Matchers.subclassesOf(ServerResource.class),
-        // Matchers.annotatedWith(Delete.class), interceptor);
     }
 }
